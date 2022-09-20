0ba74468f5fc10c5f8fb25646271da7d1141113b
abeesdev <abeesdevjs@gmail.com>
2022-09-20 17:07:06 +0700

    chang url socker

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:3080', {
+const socket = io(process.env.NEXT_BASE_API_URL as string, {
   transports: ['websocket'],
 });
 
