42c2f5c2b64a61efd5dee63b88a5ef5e0edf1972
abeesdev <abeesdevjs@gmail.com>
2022-10-02 14:05:02 +0700

    change dommain

--- next.config.js
+++ next.config.js
@@ -6,8 +6,8 @@ const nextConfig = {
     // NEXT_PUBLIC_BASE_URL: 'http://localhost:3089',
     // NEXT_PUBLIC_SOCKET_URL: 'ws://localhost:3089/graphql',
 
-    NEXT_PUBLIC_BASE_URL: 'http://34.143.180.16:3030',
-    NEXT_PUBLIC_SOCKET_URL: 'ws://34.143.180.16:3030/graphql',
+    NEXT_PUBLIC_BASE_URL: 'http://34.143.180.16:3089',
+    NEXT_PUBLIC_SOCKET_URL: 'ws://34.143.180.16:3089/graphql',
     // NEXT_BASE_API_URL: 'https://getway.abeesdev.site',
   },
 };
